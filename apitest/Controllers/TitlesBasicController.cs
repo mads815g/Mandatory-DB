ecbeb3a9040f8dea154475ac5137ff872dc76b6b
mads815g <mads61096@gmail.com>
2022-03-31 23:34:56 +0200

    Add project files.

---
+++ apitest/Controllers/TitlesBasicController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using apitest.Managers;
+using apitest.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace apitest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TitlesBasicController : ControllerBase
+    {
+        private static TitlesBasicManager _manager = new TitlesBasicManager();
+        // GET: api/<ValuesController>
+        [HttpGet("actors")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public ActionResult<IEnumerable<ActorView>> GetAllActors()
+        {
+            if (_manager.GetAllActorData() == null) return NoContent();
+            return Ok(_manager.GetAllActorData());
+        }
+
+        [HttpGet("movies")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public ActionResult<IEnumerable<ActorView>> GetAllMovies()
+        {
+            if (_manager.GetAllMovieData() == null) return NoContent();
+            return Ok(_manager.GetAllMovieData());
+        }
+
+        // GET api/<TitlesBasicController>/5
+        [HttpGet("actor{name}")]
+        public ActorView GetActor(string name)
+        {
+            return _manager.GetActorData(name);
+        }
+
+
+        [HttpGet("movie{name}")]
+        public MovieView GetMovie(string name)
+        {
+            return _manager.GetMovieData(name);
+        }
+
+        // POST api/<TitlesBasicController>
+        [HttpPost("actor")]
+        public NameBasics Post([FromBody] NameBasics newActor)
+        {
+            return _manager.AddActor(newActor);
+        }
+
+        [HttpPost("movie")]
+        public TitlesBasic Post([FromBody] TitlesBasic newMovie)
+        {
+            return _manager.AddMovie(newMovie);
+        }
+
+        // PUT api/<TitlesBasicController>/5
+        [HttpPut("actor{name}")]
+        public NameBasics Put(string name, [FromBody] NameBasics value)
+        {
+            return _manager.UpdateActor(name, value);
+        }
+
+        [HttpPut("movie{name}")]
+        public TitlesBasic Put(string name, [FromBody] TitlesBasic value)
+        {
+            return _manager.UpdateMovie(name, value);
+        }
+
+        // DELETE api/<TitlesBasicController>/5
+        [HttpDelete("actor{name}")]
+        public void DeleteActor(string name)
+        {
+            _manager.DeleteActor(name);
+        }
+
+        [HttpDelete("movie{name}")]
+        public void DeleteMovie(string name)
+        {
+            _manager.DeleteMovie(name);
+        }
+    }
+}
---
+++ apitest/Managers/TitlesBasicManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using apitest.Models;
+using Microsoft.Data.SqlClient;
+
+namespace apitest.Managers
+{
+    public class TitlesBasicManager
+    {
+        private readonly IMDBContext _context = new IMDBContext();
+
+        public List<MovieView> GetAllMovieData()
+        {
+            _context.connection = "server = localhost; database = IMDB; user id = reader123; password = 123";
+            return _context.MovieView.Take(2).ToList();
+        }
+        public List<ActorView> GetAllActorData()
+        {
+            _context.connection = "server = localhost; database = IMDB; user id = reader123; password = 123";
+            return _context.ActorView.Take(10).ToList();
+        }
+
+        public ActorView GetActorData(string name)
+        {
+            _context.connection = "server = localhost; database = IMDB; user id = reader123; password = 123";
+            return _context.ActorView.FirstOrDefault(a => a.PrimaryName.Contains(name));
+        }
+
+        public MovieView GetMovieData(string name)
+        {
+            _context.connection = "server = localhost; database = IMDB; user id = reader123; password = 123";
+            return _context.MovieView.FirstOrDefault(a => a.PrimaryTitle.Contains(name));
+        }
+
+        public NameBasics AddActor(NameBasics newActor)
+        {
+            _context.connection = "server = localhost; database = IMDB; user id = writer123; password = 123";
+            _context.NameBasics.Add(newActor);
+            _context.SaveChanges();
+            return newActor;
+        }
+
+        public TitlesBasic AddMovie(TitlesBasic newMovie)
+        {
+            _context.connection = "server = localhost; database = IMDB; user id = writer123; password = 123";
+            _context.TitlesBasic.Add(newMovie);
+            _context.SaveChanges();
+            return newMovie;
+        }
+
+        public NameBasics UpdateActor(string name, NameBasics value)
+        {
+            _context.connection = "server = localhost; database = IMDB; user id = writer123; password = 123";
+            NameBasics actor = _context.NameBasics.FirstOrDefault(a => a.PrimaryName == name);
+            if (actor == null) return null;
+            actor.BirthYear = value.BirthYear;
+            actor.DeathYear = value.DeathYear;
+            _context.SaveChanges();
+            return actor;
+        }
+
+        public TitlesBasic UpdateMovie(string name, TitlesBasic value)
+        {
+            _context.connection = "server = localhost; database = IMDB; user id = writer123; password = 123";
+            TitlesBasic movie = _context.TitlesBasic.FirstOrDefault(a => a.PrimaryTitle == name);
+            if (movie == null) return null;
+            movie.OriginalTitle = value.OriginalTitle;
+            movie.EndYear = value.EndYear;
+            movie.StartYear = value.StartYear;
+            movie.IsAdult = value.IsAdult;
+            movie.RunTimeMinutes = value.RunTimeMinutes;
+            movie.TitleType = value.TitleType;
+            _context.SaveChanges();
+            return movie;
+        }
+
+        public void DeleteActor(string name)
+        {
+            _context.connection = "server = localhost; database = IMDB; user id = writer123; password = 123";
+            NameBasics ActorToDelete = _context.NameBasics.FirstOrDefault(a => a.PrimaryName == name);
+            _context.NameBasics.Remove(ActorToDelete);
+            _context.SaveChanges();
+        }
+
+        public void DeleteMovie(string name)
+        {
+            _context.connection = "server = localhost; database = IMDB; user id = writer123; password = 123";
+            TitlesBasic MovieToDelete = _context.TitlesBasic.FirstOrDefault(a => a.PrimaryTitle == name);
+            _context.TitlesBasic.Remove(MovieToDelete);
+            _context.SaveChanges();
+        }
+    }
+}
